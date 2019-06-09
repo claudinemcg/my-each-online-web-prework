b8e75b50119195a86d72f17de4d610bb4810a062
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-09 08:11:59 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,8 @@
-def my_each 
+def my_each(array)
   
-  
- 
+  while i < array.length 
+    yield(array[i])
+    i += 1
+ end
 end
 
-
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
-end
